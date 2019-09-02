76244ec1f639e838b15fb4bafbe68f0bb27badde
DavidPinilla1 <d.pinilla@geekshubs.com>
2019-09-02 15:54:04 +0200

    added response to the DOM

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,6 +1,8 @@
-fetch('http://localhost:3000') //A J A X
-.then(res=>res.text())
-.then(res=>{
-    document.querySelector('.res').innerHTML=res
-})
-.catch(error=>console.log(error))
+function pedirAlaAPI  ()  {
+    fetch( 'http://localhost:3000' ) //A J A X
+        .then( res => res.text() )
+        .then( res => {
+            document.querySelector( '.res' ).innerHTML = res
+        } )
+        .catch( error => console.log( error ) )
+}
