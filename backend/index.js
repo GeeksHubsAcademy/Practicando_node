3567f547d54196626313e60c89d51e2018174d82
DavidPinilla1 <d.pinilla@geekshubs.com>
2019-09-03 12:48:42 +0200

    empezando con peticiones POST

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,12 @@ app.use( function ( req, res, next ) {
     res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
     next();
 } );
-
-app.get( '/',  ( req, res ) => res.send('hola express'))
-
+app.use(express.json()); //parsea de JSON el body de la petición 
+app.get( '/',  ( req, res ) => {
+    res.send('hola express')
+})
+app.post('/signup',(req,res)=>{
+    console.log(req.headers)
+    res.send(req.body); 
+})
 app.listen( 3000 ,()=>console.log('servidor levantado en el puerto 3000'))
