bd5c0cf18bf46bf87de9df72245cbd92effb08f7
DavidPinilla1 <d.pinilla@geekshubs.com>
2019-09-06 12:37:19 +0200

    passwords now are encrypted when you sign up, also login added

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,19 +1,27 @@
-const mongoose=require('mongoose');
-
-const userSchema=new mongoose.Schema({
-    usuario:{
-        type:String,
-        required:true,
-        unique:true,
-        index:true
+const mongoose = require( 'mongoose' );
+const bcrypt = require( 'bcrypt' );
+const userSchema = new mongoose.Schema( {
+    usuario: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true
     },
-    password:{
-        type:String,
-        required:true,
-        minlength:8
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
     }
-});
-
-
-const UserModel=mongoose.model('user',userSchema);
-module.exports=UserModel;
+} );
+userSchema.pre( 'save', function ( next ) {
+    const user = this;
+    if ( user.isModified( 'password' ) ) {
+        bcrypt.hash( user.password, 10 )
+            .then( hash => {
+                user.password = hash;
+                return next();
+            } ).catch( err => next( err ) )
+    } else next();
+} )
+const UserModel = mongoose.model( 'user', userSchema );
+module.exports = UserModel;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,17 +1,30 @@
-const router=require('express').Router();
+const router = require( 'express' ).Router();
 const UserModel = require( '../models/User' );
-
+const bcrypt = require( 'bcrypt' )
 router.get( '/', ( req, res ) => { //READ
     UserModel.find( {} ).then( users => res.send( users ) ).catch( console.log )
 } );
 router.post( '/signup', async ( req, res ) => { //CREATE
     try {
-        console.log(req.body);
         const user = await new UserModel( req.body ).save()
         res.send( user );
     } catch ( error ) {
         console.log( error );
         res.status( 500 ).send( 'Ha habido un problema al registrar el usuario' );
+    }
+} )
+router.post( '/login', async ( req, res ) => {
+    try {
+        const user = await UserModel.findOne( { $or: [ 
+            { usuario: req.body.usuario }, { email: req.body.email } 
+        ] } ); 
+        if ( !user ) return res.status( 400 ).send( 'Usuario o contraseña incorrectos' )
+        const isMatch = await bcrypt.compare( req.body.password, user.password )
+        if ( !isMatch ) return res.status( 400 ).send( 'Usuario o contraseña incorrectos' )
+        res.json( user )
+    } catch ( error ) {
+        console.log( error );
+        res.status( 500 ).send( 'ha habido un problema con el servidor' )
     }
 } )
 router.patch( '/:id', async ( req, res ) => {
@@ -31,4 +44,4 @@ router.delete( '/:id', async ( req, res ) => {
         console.log( error );
     }
 } )
-module.exports=router;
+module.exports = router;
